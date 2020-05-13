5ee39d3d24febeacf21cec9375c4570d72f11ad2
Bosco <bosco11jico100@gmail.com>
2020-05-12 23:40:19 -0600

    g
    g

--- NavegadorWeb/NavegadorWeb/Inicio.Designer.cs
+++ NavegadorWeb/NavegadorWeb/Inicio.Designer.cs
@@ -29,42 +29,31 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.newVentana = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage2.Size = new System.Drawing.Size(1507, 732);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "+";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
-            // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.newVentana);
             this.tabPage1.Controls.Add(this.button6);
             this.tabPage1.Controls.Add(this.button7);
             this.tabPage1.Controls.Add(this.button5);
             this.tabPage1.Controls.Add(this.webBrowser1);
-            this.tabPage1.Controls.Add(this.button4);
             this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button4);
             this.tabPage1.Controls.Add(this.button3);
             this.tabPage1.Controls.Add(this.button2);
             this.tabPage1.Controls.Add(this.button1);
@@ -75,40 +64,42 @@
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabPage1.Size = new System.Drawing.Size(1507, 732);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "google";
+            this.tabPage1.Text = "google.com";
             this.tabPage1.UseVisualStyleBackColor = true;
             this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
             // 
-            // button6
+            // button1
             // 
-            this.button6.Location = new System.Drawing.Point(9, 170);
-            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(100, 28);
-            this.button6.TabIndex = 9;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 39);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button5
+            // button2
             // 
-            this.button5.Image = global::NavegadorWeb.Properties.Resources.icon;
-            this.button5.Location = new System.Drawing.Point(143, 0);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(43, 39);
-            this.button5.TabIndex = 8;
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(48, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(43, 39);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // webBrowser1
+            // button3
             // 
-            this.webBrowser1.Location = new System.Drawing.Point(5, 43);
-            this.webBrowser1.Margin = new System.Windows.Forms.Padding(0);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(27, 25);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(1357, 662);
-            this.webBrowser1.TabIndex = 7;
+            this.button3.Image = global::NavegadorWeb.Properties.Resources.curve1;
+            this.button3.Location = new System.Drawing.Point(96, 0);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(43, 39);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox1
             // 
@@ -119,17 +110,36 @@
             this.textBox1.Size = new System.Drawing.Size(761, 38);
             this.textBox1.TabIndex = 3;
             // 
-            // tabControl1
+            // button4
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.ItemSize = new System.Drawing.Size(55, 21);
-            this.tabControl1.Location = new System.Drawing.Point(1, 2);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1515, 761);
-            this.tabControl1.TabIndex = 0;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(192, 0);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(43, 39);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(5, 43);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(27, 25);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1357, 662);
+            this.webBrowser1.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.Image = global::NavegadorWeb.Properties.Resources.icon;
+            this.button5.Location = new System.Drawing.Point(143, 0);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(43, 39);
+            this.button5.TabIndex = 8;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
             // 
             // button7
             // 
@@ -143,49 +153,38 @@
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
-            // button4
+            // button6
             // 
-            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
-            this.button4.Location = new System.Drawing.Point(192, 0);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(43, 39);
-            this.button4.TabIndex = 4;
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button6.Location = new System.Drawing.Point(9, 170);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 28);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
-            // button3
+            // newVentana
             // 
-            this.button3.Image = global::NavegadorWeb.Properties.Resources.curve1;
-            this.button3.Location = new System.Drawing.Point(96, 0);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(43, 39);
-            this.button3.TabIndex = 2;
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.newVentana.Location = new System.Drawing.Point(1060, 0);
+            this.newVentana.Margin = new System.Windows.Forms.Padding(4);
+            this.newVentana.Name = "newVentana";
+            this.newVentana.Size = new System.Drawing.Size(43, 39);
+            this.newVentana.TabIndex = 10;
+            this.newVentana.Text = "+";
+            this.newVentana.UseVisualStyleBackColor = true;
+            this.newVentana.Click += new System.EventHandler(this.newVentana_Click);
             // 
-            // button2
+            // tabControl1
             // 
-            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(48, 0);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(43, 39);
-            this.button2.TabIndex = 1;
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button1
-            // 
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(43, 39);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.ItemSize = new System.Drawing.Size(55, 21);
+            this.tabControl1.Location = new System.Drawing.Point(1, 2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1515, 761);
+            this.tabControl1.TabIndex = 0;
             // 
             // Inicio
             // 
@@ -209,17 +208,17 @@
 
         #endregion
 
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button newVentana;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.WebBrowser webBrowser1;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button6;
     }
 }
--- NavegadorWeb/NavegadorWeb/Inicio.cs
+++ NavegadorWeb/NavegadorWeb/Inicio.cs
@@ -16,6 +16,7 @@ namespace NavegadorWeb
     public partial class Inicio : Form
     {
         WebClient Client;
+        
         public Inicio()
         {
             InitializeComponent();
@@ -39,50 +40,19 @@ namespace NavegadorWeb
         }
 
         private void tabPage2_Click(object sender, EventArgs e)
-        { 
+        {
+            
+            
 
-            string title = "New Tab " + (tabControl1.TabCount).ToString();
-            TabPage myTabPage = new TabPage(title);
-            this.tabControl1.TabPages.Insert(1, myTabPage);
-
-            Button b = new Button();
-
-
-            b.SetBounds(0, 0, 33, 33);
-            b.Image = Properties.Resources.flcha_I;
-
-            Button g = new Button();
-            g.SetBounds(39, 0, 33, 33);
-            g.Image = Properties.Resources.flecha_D;
-
-            Button f = new Button();
-            f.SetBounds(78, 0, 33, 33);
-            f.Image = Properties.Resources.curve;
-
-            Button o = new Button();
-            o.SetBounds(117, 0, 33, 33);
-            o.Image = Properties.Resources.icon;
-
-
-            Button y = new Button();
-            y.SetBounds(156, 0, 33, 33);
-            y.Image = Properties.Resources.internet__2_;
-
-
-            TextBox tex = new TextBox();
-            tex.Location = new Point(240, 0);
-            tex.SetBounds(158, 0, 600, 600);
-
-
-            myTabPage.Controls.Add(b);
-            myTabPage.Controls.Add(g);
-            myTabPage.Controls.Add(f);
-            myTabPage.Controls.Add(o);
-            myTabPage.Controls.Add(y);
-            myTabPage.Controls.Add(tex);
+            
+            
+           // newWebBrowser.Navigate(tex.Text);
         }
 
-
+        void clickbuscar(object sender, EventArgs e)
+        {
+            //newWebBrowser.Navigate(tex.Text);
+        }
         private void button2_Click(object sender, EventArgs e)
         {
             if (webBrowser1.CanGoForward)
@@ -132,6 +102,83 @@ namespace NavegadorWeb
             WebClient cln = new WebClient();
             cln.DownloadFile("https://i1.wp.com/todoimagenescristianas.com/wp-content/uploads/2013/05/imagenes-cristianas-descargar-gratis_9.jpg", @"c:\imagenes-cristianas-descargar-gratis_9.jpg");
             //cln.DownloadFile(url, file);
+        }
+
+        private void newVentana_Click(object sender, EventArgs e)
+        {
+
+            string title = "google.com";
+            TabPage myTabPage = new TabPage(title);
+            this.tabControl1.TabPages.Insert(1, myTabPage);
+            TextBox tex = new TextBox();
+            WebBrowser newWebBrowser = new WebBrowser();
+            Button b = new Button();
+
+
+            b.SetBounds(0, 0, 33, 33);
+            b.Image = Properties.Resources.flcha_I;
+
+            Button g = new Button();
+            g.SetBounds(39, 0, 33, 33);
+            g.Image = Properties.Resources.flecha_D;
+
+            Button f = new Button();
+            f.SetBounds(78, 0, 33, 33);
+            f.Image = Properties.Resources.curve;
+
+            Button o = new Button();
+            o.SetBounds(117, 0, 33, 33);
+            o.Image = Properties.Resources.icon;
+
+
+            Button y = new Button();
+            y.SetBounds(156, 0, 33, 33);
+            y.Image = Properties.Resources.internet__2_;
+
+            Button n = new Button();
+            n.SetBounds(400, 0, 33, 33);
+            n.Image = Properties.Resources.seo_social_web_network_internet_340_icon_icons_com_61497;
+
+
+            Button newVenta = new Button();
+            newVenta.SetBounds(420, 0, 33, 33);
+
+            //TextBox tex = new TextBox();
+            tex.Location = new Point(240, 0);
+            tex.SetBounds(200, 0, 600, 600);
+            newWebBrowser.Location = new Point(5, 43);
+
+            newWebBrowser.Width = this.Width - 30;
+            newWebBrowser.Height = this.Height - 105;
+            myTabPage.Width = this.Width - 17;
+            myTabPage.Height = this.Height - 41;
+            tabControl1.Width = this.Width - 17;
+            tabControl1.Height = this.Height - 41;
+            newWebBrowser.Navigate("http://www.google.com");
+            ;
+            newWebBrowser.DocumentCompleted += delegate
+            {
+                tex.Text = newWebBrowser.Url.ToString();
+            };
+            n.Click += delegate
+            {
+                
+                newWebBrowser.Navigate(tex.Text);
+                
+                myTabPage.Text = tex.Text;
+                
+
+            };
+            
+            //n.Click += new EventHandler(clickbuscar);
+            myTabPage.Controls.Add(b);
+            myTabPage.Controls.Add(g);
+            myTabPage.Controls.Add(f);
+            myTabPage.Controls.Add(o);
+            myTabPage.Controls.Add(y);
+            myTabPage.Controls.Add(n);
+            myTabPage.Controls.Add(tex);
+            myTabPage.Controls.Add(newWebBrowser);
         }
     }
 }

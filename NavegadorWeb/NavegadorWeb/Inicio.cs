4159d23a29cebbc84a3f13761643ab6547d3829b
Ivannia Rojas <48615135+ivarojas08@users.noreply.github.com>
2020-05-21 18:46:56 -0600

    j
    m

--- NavegadorWeb/NavegadorWeb/Inicio.cs
+++ NavegadorWeb/NavegadorWeb/Inicio.cs
@@ -382,12 +382,12 @@ namespace NavegadorWeb
         private void WebBrowser1_ProgressChanged(Object sender, WebBrowserProgressChangedEventArgs e)
         {
 
-            System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
+           /* System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
             messageBoxCS.AppendFormat("{0} = {1}", "CurrentProgress", e.CurrentProgress);
             messageBoxCS.AppendLine();
             messageBoxCS.AppendFormat("{0} = {1}", "MaximumProgress", e.MaximumProgress);
             messageBoxCS.AppendLine();
-            MessageBox.Show(messageBoxCS.ToString(), "ProgressChanged Event");
+            MessageBox.Show(messageBoxCS.ToString(), "ProgressChanged Event");*/
         }
 
         //

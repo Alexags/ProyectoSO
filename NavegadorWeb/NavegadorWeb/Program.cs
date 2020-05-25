49d958ba11cabd0e7c991523a45dbcfb1c97b8fc
Ivannia Rojas <48615135+ivarojas08@users.noreply.github.com>
2020-05-24 19:24:08 -0600

    v
    v

---
+++ NavegadorWeb/NavegadorWeb/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NavegadorWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(() =>
+            {
+
+                Application.Run(new Inicio());
+            });
+            thread.ApartmentState = ApartmentState.STA;
+            thread.Start();
+        }
+    }
+}
---
+++ NavegadorWeb/NavegadorWeb/Web_Browser.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+
+namespace NavegadorWeb
+{
+    class Web_Browser
+    {
+        
+        TabPage myTabPage;
+        string title;
+        TextBox text;
+        Button b;
+        Button g;
+        Button f;
+        Button o;
+        Button y;
+        Button n;
+        Button newVenta;
+        Button l;
+
+        
+        public Web_Browser(TabControl tabControl, Form form, WebBrowser newWebBrowser)
+        {
+            
+            this.title = "google.com";
+            this.myTabPage = new TabPage(this.title);
+            tabControl.TabPages.Insert(1, this.myTabPage);
+            this.text = new TextBox();
+            
+            tabControl.SelectedTab = this.myTabPage;
+
+            this.b = new Button();
+            this.b.SetBounds(0, 0, 33, 33);
+            this.b.Image = Properties.Resources.flcha_I;
+            this.b.Click += delegate
+            {
+                if (newWebBrowser.CanGoBack)
+                    newWebBrowser.GoBack();
+            };
+
+            this.g = new Button();
+            this.g.SetBounds(39, 0, 33, 33);
+            this.g.Image = Properties.Resources.flecha_D;
+            this.g.Click += delegate
+            {
+                if (newWebBrowser.CanGoForward)
+                    newWebBrowser.GoForward();
+            };
+
+            this.f = new Button();
+            this.f.SetBounds(78, 0, 33, 33);
+            this.f.Image = Properties.Resources.curve;
+            this.f.Click += delegate
+            {
+
+                newWebBrowser.Refresh();
+            };
+
+            this.o = new Button();
+            this.o.SetBounds(117, 0, 33, 33);
+            this.o.Image = Properties.Resources.icon;
+            this.o.Click += delegate
+            {
+                newWebBrowser.Stop();
+            };
+
+            this.y = new Button();
+            this.y.SetBounds(156, 0, 33, 33);
+            this.y.Image = Properties.Resources.internet__2_;
+            this.y.Click += delegate
+            {
+                newWebBrowser.GoHome();
+            };
+
+            this.n = new Button();
+            this.n.SetBounds(770, 0, 33, 33);
+            this.n.Image = Properties.Resources.seo_social_web_network_internet_340_icon_icons_com_61497;
+
+            this.newVenta = new Button();
+            this.newVenta.SetBounds(420, 0, 33, 33);
+
+            this.text.Location = new Point(240, 0);
+            this.text.SetBounds(193, 0, 570, 150);
+            newWebBrowser.Location = new Point(5, 43);
+
+            newWebBrowser.Width = form.Width - 30;
+            newWebBrowser.Height = form.Height - 105;
+            this.myTabPage.Width = form.Width - 17;
+            this.myTabPage.Height = form.Height - 41;
+            tabControl.Width = form.Width - 17;
+            tabControl.Height = form.Height - 41;
+            newWebBrowser.Navigate("http://www.google.com");
+
+            newWebBrowser.DocumentCompleted += delegate
+            {
+                this.text.Text = newWebBrowser.Url.ToString();
+            };
+            n.Click += delegate
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://docs.oracle.com/javase/7/docs/api/java/io/StringWriter.html");
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                /*if (response.StatusCode == HttpStatusCode.OK)
+                {
+
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = null;
+
+                    if (String.IsNullOrWhiteSpace(response.CharacterSet))
+                        readStream = new StreamReader(receiveStream);
+                    else
+                        readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+
+                    string data = readStream.ReadToEnd();
+                    Console.Write(data);
+                    this.newWebBrowser.DocumentText = data;
+                    this.newWebBrowser.Navigating +=
+                        new WebBrowserNavigatingEventHandler(webBrowser1_Navigating);
+                    response.Close();
+                    readStream.Close();
+                }*/
+
+
+            };
+
+            this.l = new Button();
+            this.l.SetBounds(805, 0, 33, 33);
+            this.l.Text = "+";
+            this.l.Click += delegate
+            {
+                //llama la funcion de agregar pagina
+            };
+
+            this.myTabPage.Controls.Add(this.b);
+            this.myTabPage.Controls.Add(this.g);
+            this.myTabPage.Controls.Add(this.f);
+            this.myTabPage.Controls.Add(this.o);
+            this.myTabPage.Controls.Add(this.y);
+            this.myTabPage.Controls.Add(this.n);
+            this.myTabPage.Controls.Add(this.l);
+            this.myTabPage.Controls.Add(this.text);
+            this.myTabPage.Controls.Add(newWebBrowser);
+
+            
+
+        }
+    }
+}
+
+
